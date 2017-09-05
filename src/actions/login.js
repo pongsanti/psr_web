877e13cb853dc98b627267947fb93ebe0f66c982
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-09-05 11:57:40 +0700

    debugging promise

--- src/actions/login.js
+++ src/actions/login.js
@@ -15,7 +15,9 @@ export const loginPost = (loginData) => {
   return (dispatch) => {
     // First dispatch: the app state is updated to inform
     // that the API call is starting.
-    dispatch(login_post(loginData))
+    const dispatch_result = dispatch(login_post(loginData))
+    console.log(typeof dispatch_result);
+    console.log(dispatch_result);
 
     // The function called by the thunk middleware can return a value,
     // that is passed on as the return value of the dispatch method.
@@ -29,7 +31,17 @@ export const loginPost = (loginData) => {
     }
     return fetch('http://localhost:4567/psr/login', fetchInit)
       .then(
-        response => response.json()
+        response => {
+          const json = response.json();
+          console.log(typeof json);
+          console.log(json);
+          console.log(json.value)
+          if(response.ok) {
+            return json
+          } else {
+            throw json;
+          }
+        }
         ,
         // Do not use catch, because that will also catch
         // any errors in the dispatch and resulting render,
@@ -41,13 +53,34 @@ export const loginPost = (loginData) => {
         }
       )
       .then(json => {
+        console.log(json)
         // We can dispatch many times!
         // Here, we update the app state with the results of the API call.
         if (!isErrorObj(json)) {    
           return dispatch(login_recv(json));
-        }
-         
+        }       
+      },
+      error => {
+        console.log('error in rejected')
+        console.log(typeof error)
+        console.log(error)
+        //return dispatch(login_fail(error))
+        return error;
+      }
+      
+      ).then(resolve => {
+        console.log('third then')
+        console.log(typeof resolve)
+        console.log(resolve)
+        dispatch(login_fail(resolve));
       })
+      
+      // .catch(error => {
+      //   console.log('fail in catch')
+      //   console.log(typeof error)
+      //   console.log(error)
+      //   return dispatch(login_fail(error))
+      // })
   }
 }
 
