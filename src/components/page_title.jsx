30bb01bd0dd9414134b01bda426758b5407a7b77
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-09-09 16:43:37 +0700

    page title spaces

--- src/components/page_title.jsx
+++ src/components/page_title.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class PageTitle extends Component {
   render () {
     return (
-      <h1 className='page-title'>{this.props.header}
+      <h1 className='page-title'>{this.props.header} {'  '}
         <small>{this.props.subHeader}</small>
       </h1>
     )
