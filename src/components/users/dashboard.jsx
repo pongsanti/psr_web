2ff459dead0b89c586c965e743413cbd4e2bb27c
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-09-28 11:49:27 +0700

    no padding for map column

--- src/components/users/dashboard.jsx
+++ src/components/users/dashboard.jsx
@@ -10,7 +10,7 @@ class Dashboard extends Component {
         <PageTitle header='Dashboard' />
         <Grid>
           <Row>
-            <Col md={9}>
+            <Col md={9} style={{padding: 0}}>
               <Map />
             </Col>
             <Col md={3}>
